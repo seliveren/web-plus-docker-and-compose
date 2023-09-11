0a5ae5ad75015a0bb5d73ccf6f609e5800b1f58d
seliveren <selivanyulia@mail.ru>
2023-09-11 21:32:35 +0300

    fix: url change

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "https://localhost:4000";
+export const URL = "https://api.yul.students.nomoredomainsicu.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
